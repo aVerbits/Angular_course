700aac79783e6963c8614a5de54fbd0e1c566ae6
aVerbits <alekseiverbitski@gmail.com>
2021-04-19 17:11:27 +0300

    add Post

--- unit3/Angular-basics/src/app/app.component.ts
+++ unit3/Angular-basics/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+export interface Post{
+  title: string
+  text: string
+  id?: number 
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Angular-basics';
+  // title = 'Angular-basics';
+  posts: Post[] = [
+    {title: 'first title', text: 'first text', id: 1},
+    {title: 'second title', text: 'second text', id: 2}
+
+  ]
 }
