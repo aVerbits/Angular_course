2b522be4aef852fb48f5c50078559e7c329efa4d
aVerbits <alekseiverbitski@gmail.com>
2021-04-21 19:03:26 +0300

    add events

--- unit4/directives/src/app/directives/style.directive.ts
+++ unit4/directives/src/app/directives/style.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Renderer2 } from "@angular/core";
+import { Directive, ElementRef, HostListener, Renderer2 } from "@angular/core";
 
 
 @Directive({
@@ -9,4 +9,18 @@ constructor(private el: ElementRef, private r: Renderer2) {
     this.r.setStyle(this.el.nativeElement, 'color', 'blue')
 // el.nativeElement.style.color = 'red'
 }
+
+    @HostListener('click', ['$event.target']) onClick(event: Event){
+        console.log(event)
+    }
+
+    @HostListener('mouseenter') onEnter() {
+        this.r.setStyle(this.el.nativeElement, 'color', 'green')
+
+    }
+
+    @HostListener('mouseleave') onLeave() {
+        this.r.setStyle(this.el.nativeElement, 'color', null)
+
+    }
 }
