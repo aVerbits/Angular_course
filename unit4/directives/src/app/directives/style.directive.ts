d8e73cee16eaefc536da300449ba871a9e5118c2
aVerbits <alekseiverbitski@gmail.com>
2021-04-22 22:05:47 +0300

    add Sending parameters to a directive

--- unit4/directives/src/app/directives/style.directive.ts
+++ unit4/directives/src/app/directives/style.directive.ts
@@ -1,26 +1,31 @@
-import { Directive, ElementRef, HostListener, Renderer2 } from "@angular/core";
+import { Directive, ElementRef, HostListener, Input, Renderer2 } from "@angular/core";
 
 
 @Directive({
     selector: '[appStyle]'
 })
 export class StyleDirective {
-constructor(private el: ElementRef, private r: Renderer2) {
-    this.r.setStyle(this.el.nativeElement, 'color', 'blue')
+
+    @Input('appStyle') color:string = 'blue'
+    @Input() fontWeight:string = 'normal'
+
+    constructor(private el: ElementRef, private r: Renderer2) {
+    // this.r.setStyle(this.el.nativeElement, 'color', 'blue')
 // el.nativeElement.style.color = 'red'
 }
-
-    @HostListener('click', ['$event.target']) onClick(event: Event){
+   @HostListener('click', ['$event.target']) onClick(event: Event){
         console.log(event)
     }
 
     @HostListener('mouseenter') onEnter() {
-        this.r.setStyle(this.el.nativeElement, 'color', 'green')
+        this.r.setStyle(this.el.nativeElement, 'color', this.color)
+        this.r.setStyle(this.el.nativeElement, 'fontWeight', this.fontWeight)
 
     }
 
     @HostListener('mouseleave') onLeave() {
         this.r.setStyle(this.el.nativeElement, 'color', null)
+        this.r.setStyle(this.el.nativeElement, 'fontWeight', null)
 
     }
 }
